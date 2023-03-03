ad5f21279400d75dc5868659cfe401fb7d405f3e
Sello98 <sellomantse18@gmail.com>
2023-03-03 11:39:13 -0800

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: 'the program's description'
- * @parameter: describe the parameter
- *
- * Return: Always 0 (Success)
- */
-
 int main(void)
 {
 int n;
@@ -20,13 +11,14 @@ p = &n;
 /*
 * write your line of code here...
 * Remember:
-* -you are not allowed to use a
-*  -you are not allowed to modify p
-*  -only one statement
-*  you are not allowed to code anything else than this line of code
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
 */
 *p = 98;
-/* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
+
