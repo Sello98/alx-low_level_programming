4b0a8b8a1940afa87fdce3460af99172ec5ebdda
Sello98 <sellomantse18@gmail.com>
2023-04-10 19:15:07 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,10 +6,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	int a, count = 0;
+	int a count = 0;
 	unsigned long int current;
 
-	for (a = 63; i >= 0; a--)
+	for (a = 63; a >= 0; a--)
 	{
 		current = n >> a;
 
